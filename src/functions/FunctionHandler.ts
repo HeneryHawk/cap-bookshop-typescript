cfa563ee275427ffe02f5b7a77a6cdb5e5825c9c
Simon Gaudek <simon.gaudek@frauscher.com>
2022-07-10 12:44:52 +0200

    fix(service): Decorate action submitOrder with @Action

--- src/functions/FunctionHandler.ts
+++ src/functions/FunctionHandler.ts
@@ -1,5 +1,5 @@
 import cds from "@sap/cds";
-import { Handler, Req, Func, Param } from "cds-routing-handlers";
+import { Handler, Req,  Param, Action } from "cds-routing-handlers";
 import { CatalogService } from "../entities";
 
 /**
@@ -12,12 +12,13 @@ import { CatalogService } from "../entities";
 export class FunctionHandler {
     // The @Param decorator imports a url-parameter defined in your function definition
     // The @Req decorator import the current request object
-    @Func(CatalogService.ActionSubmitOrder.name)
+    @Action(CatalogService.ActionSubmitOrder.name)
     public async submitOrder(
         @Param(CatalogService.ActionSubmitOrder.paramBook) bookId: CatalogService.IBook["ID"],
         @Param(CatalogService.ActionSubmitOrder.paramAmount) amount: number,
         @Req() req: any
     ): Promise<void> {
+        console.log(`Action ${CatalogService.ActionSubmitOrder.name} called`);
         const n = await cds
             .update(CatalogService.SanitizedEntity.Book)
             .with({ stock: { "-=": amount } })
